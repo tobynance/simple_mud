1632733ba5add2478867f274f9b9797e96a52be9
Toby Nance <toby.nance@gmail.com>
2015-02-10 21:20:42 -0600

    updated test_player.py to get the current tests working, and to add some more tests.

--- test/test_player.py
+++ test/test_player.py
@@ -1,6 +1,5 @@
 import unittest
-from attributes import AttributeSet, Attributes, PlayerRank
-from item import Item, ItemDatabase
+from attributes import PlayerRank
 from player import Player, PlayerDatabase
 
 
@@ -10,6 +9,12 @@ class PlayerTest(unittest.TestCase):
     def setUp(self):
         self.player = Player()
         self.player.name = "jerry_john"
+        self.player.password = "supersecret"
+        self.player.money = 1823
+        self.player.experience = 17
+        self.player.rank = PlayerRank.MODERATOR
+        self.player.room = 3
+        self.player.stat_points = 16
         self.player.attributes.AGILITY = 3
         self.player.attributes.MAX_HIT_POINTS = 12
         self.player.attributes.STRIKE_DAMAGE = 2
@@ -17,16 +22,16 @@ class PlayerTest(unittest.TestCase):
         self.player_data = {"armor": None,
                             "attributes": {1: 0, 2: 0, 3: 3, 4: 12, 5: 0, 6: 0, 7: 2, 8: 0, 9: 1},
                             "base_attributes": {1: 1, 2: 1, 3: 1, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0},
-                            "experience": 0,
+                            "experience": 17,
                             "inventory": [],
                             "level": 1,
-                            "money": 0,
+                            "money": 1823,
                             "name": "jerry_john",
                             "next_attack_time": 0,
-                            "password": "UNDEFINED",
-                            "rank": 1,
-                            "room": 0,
-                            "stat_points": 18,
+                            "password": "supersecret",
+                            "rank": 2,
+                            "room": 3,
+                            "stat_points": 16,
                             "weapon": None}
 
     ####################################################################
@@ -38,54 +43,57 @@ class PlayerTest(unittest.TestCase):
     def test_deserialize_from_dict(self):
         player = Player.deserialize_from_dict(self.player_data)
 
-        self.assertEqual(item.id, 1)
-        self.assertEqual(item.name, "LIES!!!@~")
-        self.assertEqual(item.type, ItemType.HEALING)
-        self.assertEqual(item.min, 0)
-        self.assertEqual(item.max, 0)
-        self.assertEqual(item.price, 1)
-        self.assertEqual(item.speed, 0)
-        self.assertEqual(item.attributes.STRENGTH, 0)
-        self.assertEqual(item.attributes.HEALTH, 0)
-        self.assertEqual(item.attributes.AGILITY, 0)
-        self.assertEqual(item.attributes.MAX_HIT_POINTS, 0)
-        self.assertEqual(item.attributes.ACCURACY, 0)
-        self.assertEqual(item.attributes.DODGING, 0)
-        self.assertEqual(item.attributes.STRIKE_DAMAGE, 0)
-        self.assertEqual(item.attributes.DAMAGE_ABSORB, 0)
-        self.assertEqual(item.attributes.HP_REGEN, 0)
+        self.assertEqual(player.name, "jerry_john")
+        self.assertEqual(player.password, "supersecret")
+        self.assertEqual(player.experience, 17)
+        self.assertEqual(player.level, 1)
+        self.assertEqual(player.rank, PlayerRank.MODERATOR)
+        self.assertEqual(player.stat_points, 16)
+        self.assertEqual(player.armor, None)
+        self.assertEqual(player.weapon, None)
+        self.assertEqual(player.attributes.STRENGTH, 0)
+        self.assertEqual(player.attributes.HEALTH, 0)
+        self.assertEqual(player.attributes.AGILITY, 3)
+        self.assertEqual(player.attributes.MAX_HIT_POINTS, 12)
+        self.assertEqual(player.attributes.ACCURACY, 0)
+        self.assertEqual(player.attributes.DODGING, 0)
+        self.assertEqual(player.attributes.STRIKE_DAMAGE, 2)
+        self.assertEqual(player.attributes.DAMAGE_ABSORB, 0)
+        self.assertEqual(player.attributes.HP_REGEN, 1)
 
-        item_data = {"id": 55,
-                     "name": "Platemail Armor of Power",
-                     "type": "ARMOR",
-                     "min": 0,
-                     "max": 0,
-                     "speed": 0,
-                     "price": 15000,
-                     "STRENGTH": 0,
-                     "HEALTH": 0,
-                     "AGILITY": 0,
-                     "MAX_HIT_POINTS": 0,
-                     "ACCURACY": 10,
-                     "DODGING": 60,
-                     "STRIKE_DAMAGE": 10,
-                     "DAMAGE_ABSORB": 5,
-                     "HP_REGEN": 0}
+        self.assertEqual(player.base_attributes.STRENGTH, 1)
+        self.assertEqual(player.base_attributes.HEALTH, 1)
+        self.assertEqual(player.base_attributes.AGILITY, 1)
+        self.assertEqual(player.base_attributes.MAX_HIT_POINTS, 0)
+        self.assertEqual(player.base_attributes.ACCURACY, 0)
+        self.assertEqual(player.base_attributes.DODGING, 0)
+        self.assertEqual(player.base_attributes.STRIKE_DAMAGE, 0)
+        self.assertEqual(player.base_attributes.DAMAGE_ABSORB, 0)
+        self.assertEqual(player.base_attributes.HP_REGEN, 0)
 
-        item = Item.deserialize_from_dict(item_data)
-        self.assertEqual(item.id, 55)
-        self.assertEqual(item.name, "Platemail Armor of Power")
-        self.assertEqual(item.type, ItemType.ARMOR)
-        self.assertEqual(item.min, 0)
-        self.assertEqual(item.max, 0)
-        self.assertEqual(item.price, 15000)
-        self.assertEqual(item.speed, 0)
-        self.assertEqual(item.attributes.STRENGTH, 0)
-        self.assertEqual(item.attributes.HEALTH, 0)
-        self.assertEqual(item.attributes.AGILITY, 0)
-        self.assertEqual(item.attributes.MAX_HIT_POINTS, 0)
-        self.assertEqual(item.attributes.ACCURACY, 10)
-        self.assertEqual(item.attributes.DODGING, 60)
-        self.assertEqual(item.attributes.STRIKE_DAMAGE, 10)
-        self.assertEqual(item.attributes.DAMAGE_ABSORB, 5)
-        self.assertEqual(item.attributes.HP_REGEN, 0)
+
+########################################################################
+class PlayerDatabaseTest(unittest.TestCase):
+    ####################################################################
+    def test_load(self):
+        self.fail()
+
+    ####################################################################
+    def test_save(self):
+        self.fail()
+
+    ####################################################################
+    def test_add_player(self):
+        self.fail()
+
+    ####################################################################
+    def test_find_active(self):
+        self.fail()
+
+    ####################################################################
+    def test_find_logged_in(self):
+        self.fail()
+
+    ####################################################################
+    def test_log_out(self):
+        self.fail()

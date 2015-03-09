37febffdd377c43097a8e3a5d5b33c1afeb60bad
Toby Nance <toby.nance@gmail.com>
2015-03-08 19:04:25 -0500

    reworked recreate_migrations.py to work with postgres and sqlite3

--- recreate_migrations.py
+++ recreate_migrations.py
@@ -3,7 +3,6 @@ import os
 import glob
 import subprocess
 
-DB_FILE = "db.sqlite3"
 MIGRATION_FOLDER = os.path.join("mud", "migrations")
 DATA_MIGRATION_FILE = os.path.join(MIGRATION_FOLDER, "0002_initial_data.py")
 TEMP_DATA_MIGRATION_FILE = DATA_MIGRATION_FILE[:-3]
@@ -12,14 +11,12 @@ PYTHON = sys.executable
 
 ########################################################################
 def main():
+    subprocess.call([PYTHON, "manage.py", "flush", "--noinput"])
     if os.path.exists(DATA_MIGRATION_FILE):
         os.rename(DATA_MIGRATION_FILE, TEMP_DATA_MIGRATION_FILE)
     else:
         raise Exception("No Data migration file found: %s" % DATA_MIGRATION_FILE)
     try:
-        if os.path.exists(DB_FILE):
-            os.remove(DB_FILE)
-
         for filename in (glob.glob1(MIGRATION_FOLDER, "*.py") + glob.glob1(MIGRATION_FOLDER, "*.pyc")):
             if filename == "__init__.py":
                 continue
